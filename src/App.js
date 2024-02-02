8e7d2fcfd1dc13a7e20552986045493961936187
rashika <rashika2708@gmail.com>
2024-02-02 11:34:05 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.error("Error Fetching data: ", err));
+  }, []);
+
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column", // Updated to column layout
+    alignItems: "center",
+    height: "100vh",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {countries
+          .filter((country) =>
+            country.name.common
+              .toLowerCase()
+              .includes(searchInput.toLowerCase())
+          )
+          .map((country) => (
+            <div key={country.cca3} style={cardStyle} className="countryCard">
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                style={imageStyle}
+              />
+              <h2>{country.name.common}</h2>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default App;
